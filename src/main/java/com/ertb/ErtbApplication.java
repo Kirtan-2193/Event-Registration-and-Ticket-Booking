2ce0368622c031fd74b47c028d1d342372373c09
KirtanBhavsar2193 <kirtanbhavsar21@gmail.com>
2025-10-01 16:52:49 +0530

    first commit

---
+++ src/main/java/com/ertb/ErtbApplication.java
@@ -0,0 +1,13 @@
+package com.ertb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErtbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErtbApplication.class, args);
+	}
+
+}
